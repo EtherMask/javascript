9bdbe2ad0eab00a2d1a013528665cdbb8b776104
ict-17 <ict-17@DESKTOP-PC34DF0>
2024-10-24 17:50:47 +0900

    j쿼리 전송

--- src/main/webapp/4_javascript_class/06_function/js/arg_func.js
+++ src/main/webapp/4_javascript_class/06_function/js/arg_func.js
@@ -2,7 +2,8 @@
 function arrayProcess( data, f ) {
 	for( key=0; key < data.length; key++ ){
 	//for(var key in data ){
-		f(  data[key].value );
+		f(  data[key].value );	// f에 sum이 들어오면 sum함수를 부르고
+								// square가 들어오면 square를 함수를 부른다
 	}
 }
 
